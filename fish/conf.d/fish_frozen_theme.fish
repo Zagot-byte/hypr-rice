0406820b396b45c33ec6a7ae1ab00322dc1ee68d
Zagot-byte <zagot316@gmail.com>
2026-03-03 17:00:31 +0530

    added wayabr,kitty,rofi,Thunar,fish

---
+++ fish/conf.d/fish_frozen_theme.fish
@@ -0,0 +1,45 @@
+# Don't edit this file, as it will be written by the web-config tool (`fish_config`).
+# To customize your theme, delete this file and see
+#     help interactive#syntax-highlighting
+# or
+#     man fish-interactive | less +/^SYNTAX.HIGHLIGHTING
+# for appropriate commands to add to ~/.config/fish/config.fish instead.
+
+set --global fish_color_autosuggestion 424242
+set --global fish_color_cancel --reverse
+set --global fish_color_command ff7575
+set --global fish_color_comment 424242 --italics
+set --global fish_color_cwd 5e81ac
+set --global fish_color_cwd_root bf616a
+set --global fish_color_end 424242
+set --global fish_color_error aa0c14 --bold
+set --global fish_color_escape e55b5b
+set --global fish_color_gray 
+set --global fish_color_history_current e5e9f0 --bold
+set --global fish_color_host a3be8c
+set --global fish_color_host_remote ebcb8b
+set --global fish_color_keyword e55b5b
+set --global fish_color_normal e0e0e0
+set --global fish_color_operator ff7575
+set --global fish_color_option 8fbcbb
+set --global fish_color_param e0e0e0
+set --global fish_color_quote 9e9e9e
+set --global fish_color_redirection e55b5b
+set --global fish_color_search_match --background=3a1f1f
+set --global fish_color_selection --background=3a1f1f
+set --global fish_color_status bf616a
+set --global fish_color_user a3be8c
+set --global fish_color_valid_path --underline=single
+set --global fish_pager_color_background 
+set --global fish_pager_color_completion e5e9f0
+set --global fish_pager_color_description ebcb8b --italics
+set --global fish_pager_color_prefix normal --bold --underline=single
+set --global fish_pager_color_progress 3b4252 --bold --background=d08770
+set --global fish_pager_color_secondary_background 
+set --global fish_pager_color_secondary_completion 
+set --global fish_pager_color_secondary_description 
+set --global fish_pager_color_secondary_prefix 
+set --global fish_pager_color_selected_background --background=434c5e
+set --global fish_pager_color_selected_completion 
+set --global fish_pager_color_selected_description 
+set --global fish_pager_color_selected_prefix 
---
+++ fish/conf.d/rose-grey.fish
@@ -0,0 +1,17 @@
+# Rose x Grey — Fish colors + prompt
+
+# Syntax highlighting
+set fish_color_normal          e0e0e0
+set fish_color_command         ff7575
+set fish_color_keyword         e55b5b
+set fish_color_quote           9e9e9e
+set fish_color_redirection     e55b5b
+set fish_color_end             424242
+set fish_color_error           aa0c14 --bold
+set fish_color_param           e0e0e0
+set fish_color_comment         424242 --italics
+set fish_color_selection       --background=3a1f1f
+set fish_color_search_match    --background=3a1f1f
+set fish_color_operator        ff7575
+set fish_color_escape          e55b5b
+set fish_color_autosuggestion  424242
